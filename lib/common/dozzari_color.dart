4b5d81022bb433cfbedbb38cef6e77a4c9baed42
dongsu_ko <127917642+kdongsu5509@users.noreply.github.com>
2024-09-26 11:02:56 +0900

    [add] const color and font

---
+++ lib/common/dozzari_color.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+Color BRAND_PRIMARY_COLOR = Color(0xFF734B34);
+Color BRAND_SECONDARY_COLOR = Color(0xFFF28705);
+
+Color STATE_INFO_COLOR = Color(0xFF3281EA);
+Color STATE_SUCCESS_COLOR = Color(0xFF2AAF61);
+Color STATE_WARNING_COLOR = Color(0xFFE3B83C);
+Color STATE_ERROR_COLOR = Color(0xFFE95658);
+
+Color BG_BLACK = Color(0xFF000000);
+Color BG_BLACK2 = Color(0xFFF1F1F5);
+Color BG_BLACK3 = Color(0xFFF7F7F9);
+Color BG_WHITE = Color(0xFFFFFFFF);
+
+Color LINE_BLACK = Color(0xFF000000);
+Color LINE_GRAY1 = Color(0xFFEBEBEB); // is will be replacdd by six length grey color hex code
+Color LINE_GRAY3 = Color(0xFFF7F7F9);
+
+Color FONT_BLACK = Color(0xFF343434);
+Color FONT_BLACK2 = Color(0xFF4F4F4F);
+Color FONT_GRAY3 = Color(0xFF777777);
+Color FONT_GRAY4 = Color(0xFFBDBDBD);
